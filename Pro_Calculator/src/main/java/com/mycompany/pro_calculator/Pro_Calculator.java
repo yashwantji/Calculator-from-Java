2f16bfa4a16b35c5c824690369580ddb50681b82
Yashwant Gupta <112464875+yashwantji@users.noreply.github.com>
2024-09-03 21:49:07 +0530

    Add files via upload

---
+++ Pro_Calculator/src/main/java/com/mycompany/pro_calculator/Pro_Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pro_calculator;
+
+/**
+ *
+ * @author yashw
+ */
+public class Pro_Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
